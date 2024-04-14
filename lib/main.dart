3f490a918162b8498e3b95020f33defb8e5f695a
JunhaPark96 <njk116@gmail.com>
2024-04-14 21:50:10 +0900

    기본 레이아웃 구성

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // 라이브러리 Import
 
 // => : fat arrow - 호출을 의미
   // runApp은 widget을 argument로 가져야함
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); // custom widget을 argument로 넣어주기
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,9 +10,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First app',
       theme: ThemeData(
-        primarySwatch: Colors.blue // 기본 색상 견본
+        primarySwatch: Colors.amber // 기본 색상 견본
       ), // closing label: 위젯의 끝을 알려줌
-    home: MyHomePage(),
+      home: MyHomePage(), // home 위젯이 앱이 실행되면 제일 먼저 보여지는 경로이기 때문에 무조건 필요
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( // 발판 만들기 - 빈 도화지
+      appBar: AppBar(
+        title: Text('First app'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello')
+          ],
+        ),
+      ),
     );
   }
 }
