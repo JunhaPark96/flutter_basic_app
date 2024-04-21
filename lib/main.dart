2fd6987792b18f510060700eae087e78e4c866f8
JunhaPark96 <njk116@gmail.com>
2024-04-21 19:57:48 +0900

    Drawer 메뉴 만들기1

--- lib/main.dart
+++ lib/main.dart
@@ -26,12 +26,13 @@ class MyPage extends StatelessWidget {
         centerTitle: true,
         elevation: 0.0,
         // 간단한 위젯이나 아이콘을 appbar 타이틀 왼쪽에 위치시키는 역할
-        leading: IconButton(
-          icon: Icon(Icons.menu), 
-          onPressed: () { 
-            print('menu button is clicked');
-           },
-        ),
+        // leading: IconButton(
+        //   icon: Icon(Icons.menu), 
+        //   onPressed: () { 
+        //     print('menu button is clicked');
+        //    },
+        // ),
+        // actions: 복수의 아이콘 버튼 등을 오른쪽에 배치
         actions: <Widget>[
           IconButton(
           icon: Icon(Icons.shopping_cart), 
@@ -46,6 +47,35 @@ class MyPage extends StatelessWidget {
            },
         ),
         ],
+      ),
+      // widget 기본 클래스
+      drawer: Drawer(
+        // padding 이나 margin에 신경쓰지 않고 배치할 수 있도록 ListTile widget제공
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            // required argument 필수
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/kopo.jpg'),
+                backgroundColor: Colors.white,
+              ),
+              accountName: Text('KOPO'),
+              accountEmail: Text('kopo@office.kopo.ac.kr'),
+              // 클릭 시 기능 수행 디테일
+              onDetailsPressed: (){
+                print('Kopo friends');              
+                },
+                decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0),
+                  )
+                ),
+            ),
+          ],
+        ), 
       ),
     );
   }
