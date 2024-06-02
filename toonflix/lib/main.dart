8ab30d1697969d7ee7df15344f3aae508e0be4c2
JunhaPark96 <njk116@gmail.com>
2024-06-02 22:16:59 +0900

    Dribble Example: Button Selection

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       // flutter Rule: 모든 화면은 scaffold가 필요 함.
       home: Scaffold(
@@ -16,6 +16,8 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column (
+            crossAxisAlignment: CrossAxisAlignment.start,
+            
             children: [
               SizedBox(
                 height: 80,
@@ -43,9 +45,60 @@ class App extends StatelessWidget {
                   ],)
                 ],
               ),
+
+              SizedBox(
+                height: 120,
+                ),
+
+                Text('Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Color.fromRGBO(255, 255, 255, 0.788),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+
+                Text('\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(45),
+                        color: Colors.amber,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 15, 
+                          horizontal: 40
+                        ),
+                        child: Text('Transfer',
+                          style: TextStyle(
+                            fontSize: 22,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                
             ],
+
           ),
-        )
+        ),
+
+
       ),
     );
   }
