ffadaa0b39bf3c02e5f05e1af9b851bcdc4e3736
JunhaPark96 <njk116@gmail.com>
2024-06-02 21:10:33 +0900

    flutter: named parameter & positional parameter

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name; // ?: 파라미터를 가질 수도 안 가질 수도 있음.
+
+  Player({required this.name}); // named parameter
+}
+
 void main() {
+  var junha = Player(name: "junha");
   runApp(App()); // 모든 앱의 root -> material || cupertinoApp을 return 해야 함
 }
 
@@ -13,11 +20,12 @@ class App extends StatelessWidget {
       // flutter Rule: 모든 화면은 scaffold가 필요 함.
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Title'),
+          elevation: 100,
+          title: Text('Title'), // position parameter
         ),
         body: Center(
           child: Text('Hello world'),
-        )
+        ),
       ),
     );
   }
