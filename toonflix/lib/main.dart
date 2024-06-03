5ab9877b098e592a516733a76c212821ffdb4f77
JunhaPark96 <njk116@gmail.com>
2024-06-03 22:12:48 +0900

    FEAT: settings.json flutter settings added

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,104 +1,99 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App()); // 모든 앱의 root -> material || cupertinoApp을 return 해야 함
+  runApp(const App()); // 모든 앱의 root -> material || cupertinoApp을 return 해야 함
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       // flutter Rule: 모든 화면은 scaffold가 필요 함.
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40),
-          child: Column (
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 80,
               ),
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                    Text('Hey, Salena',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w800,
-          
-                    ),
-                    ),
-                    Text('Welcome back',
-                      style: TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 0.799),
-                        fontSize: 18,
-                    ),
-                    ),
-                  ],)
+                      Text(
+                        'Hey, Salena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.799),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
-
-              SizedBox(
+              const SizedBox(
                 height: 120,
+              ),
+              const Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Color.fromRGBO(255, 255, 255, 0.788),
                 ),
-
-                Text('Total Balance',
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Color.fromRGBO(255, 255, 255, 0.788),
-                  ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 42,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-
-                Text('\$5 194 482',
-                  style: TextStyle(
-                    fontSize: 42,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(45),
-                        color: Colors.amber,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 15, 
-                          horizontal: 40
-                        ),
-                        child: Text('Transfer',
-                          style: TextStyle(
-                            fontSize: 22,
-                          ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(45),
+                      color: Colors.amber,
+                    ),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 22,
                         ),
                       ),
-                    )
-                  ],
-                ),
-                
+                    ),
+                  )
+                ],
+              ),
             ],
-
           ),
         ),
-
-
       ),
     );
   }
