5bacbb33cd343246039423150117b6996422f7a5
JunhaPark96 <njk116@gmail.com>
2024-06-16 20:51:36 +0900

    WEBTOON APP: Card

--- toonflix/lib/screens/main_screen.dart
+++ toonflix/lib/screens/main_screen.dart
@@ -29,24 +29,80 @@ class MainScreen extends StatelessWidget {
         future: webtoons, // UI그려주는 builder, 자체 await가능
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                // 동적으로 item 생성. 미리 만들어 두지 않음.
-                // print(index);
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 10,
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                )
+              ],
             );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        // 동적으로 item 생성. 미리 만들어 두지 않음.
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            // 에러의 원인
+            // 따로 User-Agent 값을 추가하지 않으면 기본값으로 'Dart/<version> (dart:io)' 가 들어감
+            // 이 값을 지우고, 브라우저에서 사용하는 값으로 바꿔줘야함. - 브라우저 값이 아니면 네이버에서 차단하는것으로 예상
+            // local 방식
+            // Image.network(
+            //   webtoon.thumb,
+            //   headers: const {
+            //     "User-Agent":
+            //         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+            //   },
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  )
+                ],
+              ),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  'Referer': 'https://comic.naver.com',
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            )
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
