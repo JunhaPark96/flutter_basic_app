dbd85fa5caf5bb4b0369aabebb4ca02be8c54823
JunhaPark96 <njk116@gmail.com>
2024-06-16 20:58:00 +0900

    WEBTOON APP: Card class 분리

--- toonflix/lib/screens/main_screen.dart
+++ toonflix/lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon.dart';
 import 'package:toonflix/services/api_service.dart';
+import 'package:toonflix/widgets/webtoon_widget.dart';
 
 class MainScreen extends StatelessWidget {
   final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
@@ -56,50 +57,8 @@ class MainScreen extends StatelessWidget {
       itemBuilder: (context, index) {
         // 동적으로 item 생성. 미리 만들어 두지 않음.
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            // 에러의 원인
-            // 따로 User-Agent 값을 추가하지 않으면 기본값으로 'Dart/<version> (dart:io)' 가 들어감
-            // 이 값을 지우고, 브라우저에서 사용하는 값으로 바꿔줘야함. - 브라우저 값이 아니면 네이버에서 차단하는것으로 예상
-            // local 방식
-            // Image.network(
-            //   webtoon.thumb,
-            //   headers: const {
-            //     "User-Agent":
-            //         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-            //   },
-            Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.5),
-                  )
-                ],
-              ),
-              child: Image.network(
-                webtoon.thumb,
-                headers: const {
-                  'Referer': 'https://comic.naver.com',
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              webtoon.title,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-            )
-          ],
-        );
+        return Webtoon(
+            title: webtoon.title, thumb: webtoon.thumb, id: webtoon.id);
       },
       separatorBuilder: (context, index) => const SizedBox(
         width: 40,
---
+++ toonflix/lib/widgets/webtoon_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Webtoon extends StatelessWidget {
+  final String title, thumb, id;
+
+  const Webtoon(
+      {super.key, required this.title, required this.thumb, required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // 에러의 원인
+        // 따로 User-Agent 값을 추가하지 않으면 기본값으로 'Dart/<version> (dart:io)' 가 들어감
+        // 이 값을 지우고, 브라우저에서 사용하는 값으로 바꿔줘야함. - 브라우저 값이 아니면 네이버에서 차단하는것으로 예상
+        // local 방식
+        // Image.network(
+        //   webtoon.thumb,
+        //   headers: const {
+        //     "User-Agent":
+        //         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        //   },
+        Container(
+          width: 250,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 15,
+                offset: const Offset(10, 10),
+                color: Colors.black.withOpacity(0.5),
+              )
+            ],
+          ),
+          child: Image.network(
+            thumb,
+            headers: const {
+              'Referer': 'https://comic.naver.com',
+            },
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        )
+      ],
+    );
+  }
+}
