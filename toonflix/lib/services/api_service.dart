948449c9aab928a1b3241c24555ba48dcb75c1b8
JunhaPark96 <njk116@gmail.com>
2024-06-16 15:54:32 +0900

    WEBTOON APP: Data Fetching

--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -1,3 +1,20 @@
+import 'package:http/http.dart' as http;
+
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev/";
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    // async programming: API 요청이 끝날 때 까지 기다리게 함
+    final response = await http.get(url); // await: async 함수 내에서만 사용
+    if (response.statusCode == 200) {
+      print("header: ${response.headers}");
+      print(
+          "==================================================================================");
+      print("body: ${response.body}");
+      return;
+    }
+    throw Error();
+  }
 }
